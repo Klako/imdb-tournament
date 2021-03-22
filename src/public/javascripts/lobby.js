ef79ddc01f6cf05bca98c7e35b83fccc0fd00bfd
Alexander Krantz <klako@hotmail.se>
2021-03-22 22:28:13 +0100

    Added 'add movie' functionality

--- src/public/javascripts/lobby.js
+++ src/public/javascripts/lobby.js
@@ -1,11 +1,51 @@
-$(function () {
+const removeMovie = function (movieId) {
+  $.ajax({
+    method: "DELETE",
+    url: "/api/rooms/" + roomId + "/movies/" + movieId
+  }).done(() => {
+    loadMovies();
+  })
+}
+
+const loadMovies = function () {
   var movielist = $("#movielist");
   $.ajax({
     method: "GET",
     url: "/api/rooms/" + roomId + "/movies"
   }).done((data) => {
+    movielist.children().remove();
     for (var movie of data) {
-      movielist.append("<li>" + movie.id + "</li>");
+      var item = `<li class="list-group-item">
+        <span>${movie.id}</span>
+        <button type="button" class="btn btn-danger float-right" id=${"removemovie-" + movie.id}>Remove</button>
+      </li>`
+      movielist.append(item);
+      $("#removemovie-" + movie.id).on("click", () => removeMovie(movie.id));
     }
+  });
+}
+
+const updateUi = function () {
+  loadMovies();
+  setTimeout(updateUi, 1000)
+}
+
+$(updateUi)
+
+$(function () {
+  var addMovieButton = $("#addmovie-button");
+  var addMovieId = $("#addmovie-id");
+  addMovieButton.on("click", () => {
+    $.ajax({
+      method: "POST",
+      url: "/api/rooms/" + roomId + "/movies",
+      data: {
+        id: addMovieId.val()
+      }
+    }).done(() => {
+      loadMovies();
+    }).fail((error) => {
+      console.log("bad movie >:(");
+    });
   });
 });
