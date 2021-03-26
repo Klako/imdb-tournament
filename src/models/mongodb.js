394ca13503403aaa4a84efbfc5170720ec2857b6
Alexander Krantz <klako@hotmail.se>
2021-03-26 18:15:11 +0100

    Add mongoose connection

--- src/models/mongodb.js
+++ src/models/mongodb.js
@@ -9,14 +9,16 @@ const mongoHost = process.env.MONGO_HOST;
 const mongoPort = process.env.MONGO_PORT;
 const mongoDatabase = process.env.MONGO_DATABASE;
 
-const uri = `mongodb://${mongoUsername}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDatabase}`;
-
-mongoose.connect(uri);
+const uri = `mongodb://${mongoUsername}:${mongoPassword}@${mongoHost}:${mongoPort}`;
 
 exports.uri = uri;
 exports.database = mongoDatabase;
-exports.mongoose = mongoose;
 exports.connect = async () => {
     var client = await mongodb.connect(uri);
     return client.db(mongoDatabase);
 };
+exports.mongoose = async () =>{
+    var connection = await mongoose.createConnection(uri);
+    connection.useDb(mongoDatabase);
+    return connection;
+}
