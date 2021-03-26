dc32514b386929aa0a6ce11cee9a9ed0cc48813e
Alexander Krantz <klako@hotmail.se>
2021-03-26 14:59:18 +0100

    Add mongoose to db model

--- src/models/mongodb.js
+++ src/models/mongodb.js
@@ -1,4 +1,5 @@
 const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 var exports = module.exports;
 
 // Conenction settings
@@ -8,10 +9,13 @@ const mongoHost = process.env.MONGO_HOST;
 const mongoPort = process.env.MONGO_PORT;
 const mongoDatabase = process.env.MONGO_DATABASE;
 
-const uri = `mongodb://${mongoUsername}:${mongoPassword}@${mongoHost}:${mongoPort}`;
+const uri = `mongodb://${mongoUsername}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDatabase}`;
+
+mongoose.connect(uri);
 
 exports.uri = uri;
 exports.database = mongoDatabase;
+exports.mongoose = () => mongoose;
 exports.connect = async () => {
     var client = await mongodb.connect(uri);
     return client.db(mongoDatabase);
